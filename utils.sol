daae73913679416b414c013470833526e33daf57
Paul O'Leary <paul@oleary.com>
2018-10-27 11:50:07 -0700

    Test adding new contracts.

---
+++ utils.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.21;
+
+contract Utils {
+    function getBalance(address addr) public constant returns (uint256) {
+        return addr.balance;
+    }
+}
+
+contract Verifier {
+    function recoverAddr(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) returns (address) {
+        return ecrecover(msgHash, v, r, s);
+    }
+
+    function isSigned(address _addr, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) returns (bool) {
+        return ecrecover(msgHash, v, r, s) == _addr;
+    }
+}

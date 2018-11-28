d2e0ae5fdbabb2208f11f62e1c4d55eb1ec08a71
Marc-Olivier Lusinchi <marc-olivier@Marc-Oliviers-MacBook-Pro.local>
2018-11-28 11:40:32 -0800

    passed solhint, adds a custom description of published state

--- sample_content_helloworld.sol
+++ sample_content_helloworld.sol
@@ -1,39 +1,49 @@
-pragma solidity ^0.4.21;
+pragma solidity 0.4.21;
 
-import './library.sol';
+import {Content} from "./content.sol";
+import {BaseContent} from "./base_content.sol";
 
-contract HelloWorld is CustomContract
-{
+
+contract SampleContentHelloWorld is Content {
 
     event HelloWorldEvent(string message, address stakeHolder0);
 
     uint256 public credit = 1 * 1000000000000000000;
 
+    bytes32 public constant STATUS_PUBLISHED = "Ready";
+
+    function runDescribeStatus(int status_code) public pure returns (bytes32) {
+        if (status_code == 0) {
+            return STATUS_PUBLISHED;
+        }
+        return 0x0;
+    }
+
     function runAccess(
-        uint256 /* request_ID */,
-        uint8 /* level */,
-        bytes32[] /* custom_values */,
-	address[] stake_holders) public payable returns(uint)
+        uint256, /* request_ID */
+        uint8, /* level */
+        bytes32[], /* custom_values */
+        address[] stake_holders
+    )
+        public payable returns(uint)
     {
-	if (stake_holders.length > 0) {
+        if (stake_holders.length > 0) {
 
             emit HelloWorldEvent("Hi there", stake_holders[0]);
 
             // Pay the stake holder a small amount
-	    stake_holders[0].transfer(credit);
-	    return 0;
+            stake_holders[0].transfer(credit);
+            return 0;
 
         } else {
 
             emit HelloWorldEvent("Hi there - no stake holders", 0x0);
-	    return 1;
+            return 1;
 
         }
     }
 
-    function setCredit(
-        uint256 newCredit) public onlyOwner
-    {
+    function setCredit(uint256 newCredit) public onlyOwner {
         credit = newCredit;
     }
 

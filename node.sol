0255995ffa7139d081d460ffe32c210242ccdce8
Paul O'Leary <paul@oleary.com>
2019-04-05 17:16:42 -0700

    Dial initial account payment down to .1 ETH.

--- node.sol
+++ node.sol
@@ -19,7 +19,7 @@ contract Node is Ownable {
 
 
     function log(string label) public onlyOwner {
-        owner.transfer(1000000000000000000); // 1 ETH
+        owner.transfer(100000000000000000); // .1 ETH
         emit Log(label);
     }
 }

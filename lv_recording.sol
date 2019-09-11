5870cc6cf9a943a99fa4ec0f89ec93f85ed2cd05
Marc-Olivier Lusinchi <marc-olivier@Marc-Oliviers-MacBook-Pro.local>
2019-09-10 21:44:39 -0700

    enable manual setting of recording stream reference to allow manual instanciation

--- lv_recording.sol
+++ lv_recording.sol
@@ -5,17 +5,51 @@ import {BaseContent} from "./base_content.sol";
 import {BaseLibrary} from "./base_library.sol";
 
 
+/* -- Revision history --
+LvProvider20190907122400ML: First versioned released
+LvProvider20190910171300ML: Makes recordingStreams public.
+*/
+
+contract LvProvider is Content {
+
+    bytes32 public version = "LvProvider20190910171300ML"; //class name (max 16), date YYYYMMDD, time HHMMSS and Developer initials XX
+
+    mapping (address => bool) public recordingStreams;
+
+    event AuthorizeRecording(address stream, address accessor, bool decision);
+    event EnableRecording(address stream);
+    event DisableRecording(address stream);
+
+    function authorizeRecording(address stream, address accessor) returns (bool) {
+        bool decision = recordingStreams[stream];
+        emit AuthorizeRecording(stream, accessor, decision);
+        return decision;
+    }
+
+    function enableRecording(address stream) onlyOwner {
+        recordingStreams[stream] = true;
+        emit EnableRecording(stream);
+    }
+
+    function disableRecording(address stream) onlyOwner {
+        recordingStreams[stream] = false;
+        emit DisableRecording(stream);
+    }
+}
+
+
 
 /* -- Revision history --
 LvRecStream20190812201700ML: First versioned released
 LvRecStream20190823104800ML: Adding fields to store stream handle, and exposing start to stop time.
 LvRecStream20190825165500ML: Adding stream-wide event logging of recordings.
+LvRecStream20190907125000ML: Adding provider control
+LvRecStream20190910192800ML: Adding recordingStream override
 */
-
 
 contract LvRecordableStream is Content {
 
-    bytes32 public version = "LvRecStream20190825165500ML"; //class name (max 16), date YYYYMMDD, time HHMMSS and Developer initials XX
+    bytes32 public version = "LvRecStream20190910192800ML"; //class name (max 16), date YYYYMMDD, time HHMMSS and Developer initials XX
 
     uint public startTime;
     uint public endTime;
@@ -23,6 +57,8 @@ contract LvRecordableStream is Content {
     string public handle;
 
     address public recordingStream; //the content object for the recordable stream
+
+    address public provider;
 
     event CreateRecording(address recObj, address recContract);
     event SetRecordingTimes(address recObj, uint recStartTime, uint recEndTime);
@@ -32,16 +68,28 @@ contract LvRecordableStream is Content {
     event StopStream();
 
     constructor() public payable {
-        recordingStream = msg.sender;
+        if (msg.sender != tx.origin) {
+            recordingStream = msg.sender;
+        }
         startTime = 0;
         endTime = 0;
         handle = "";
+    }
+
+    function setRecordingStream(address stream) public onlyOwner  { //only required if contract is instanciated manually, otherwise it is set correctly in the constructor
+       recordingStream = stream;
     }
 
     // When a recordable stream is created a contract is created to track copies of that recording
     function runCreate() public payable returns (uint) {
 
         if (recordingEnabled) {
+            if (provider != 0x0) {
+                LvProvider provObj = LvProvider(provider);
+                if (provObj.authorizeRecording(recordingStream, tx.origin) == false){
+                    return 1; //Unauthorized recording, this will cause the content object to not be created, hence the denial event won't be committed to the blockchain
+                }
+            }
             address instanceAddress = new LvRecording();
             LvRecording rec = LvRecording(instanceAddress);
             rec.setContentAddress(msg.sender);
@@ -56,6 +104,12 @@ contract LvRecordableStream is Content {
 
     function enableRecording() public onlyOwner  {
         recordingEnabled = true;
+    }
+
+    function setProvider(address _provider) public onlyOwner  {
+        provider = _provider;
+        LvProvider provObj = LvProvider(provider);
+        provObj.enableRecording(recordingStream);
     }
 
     function startStream(string _handle) public onlyOwner  {

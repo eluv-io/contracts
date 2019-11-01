8e6ca27d1e7dca6c244508a2e257569b0c744429
Marc-Olivier Lusinchi <marc-olivier@Marc-Oliviers-MacBook-Pro.local>
2019-10-31 17:04:53 -0700

    adds request timestamp to events

--- base_content_space.sol
+++ base_content_space.sol
@@ -433,26 +433,29 @@ contract BaseContentFactory is Ownable {
 
 //BaseCtFactoryXt20191031115100PO: adds support for custom contract
 //BaseCtFactoryXt20191031153200ML: passes accessor to the runAccess via the addresses array
+//BaseCtFactoryXt20191031170400ML: adds request timestamp to event
 
 contract BaseContentFactoryExt is BaseContentFactory {
 
-    bytes32 public version ="BaseCtFactoryXt20191031153200ML"; //class name (max 16), date YYYYMMDD, time HHMMSS and Developer initials XX
+    bytes32 public version ="BaseCtFactoryXt20191031170400ML"; //class name (max 16), date YYYYMMDD, time HHMMSS and Developer initials XX
 
     // TODO: naming this the same as the event in BaseContentObject ...?
     event AccessRequest(
+        uint256 timestamp,
         address libraryAddress,
         address contentAddress,
         address userAddress,
         bytes32 contextHash,
-        uint64 timestamp
+        uint64 request_timestamp
     );
 
     event AccessComplete(
+        uint256 timestamp,
         address libraryAddress,
         address contentAddress,
         address userAddress,
         bytes32 contextHash,
-        uint64 timestamp
+        uint64 request_timestamp
     );
 
     uint32 public constant OP_ACCESS_REQUEST = 1;
@@ -475,7 +478,7 @@ contract BaseContentFactoryExt is BaseContentFactory {
             Content c;
             // require(msg.sender == owner || cobj.addressKMS() == msg.sender);
             if (_opCodes[i] == OP_ACCESS_REQUEST) {
-                emit AccessRequest(cobj.libraryAddress(), _contentAddrs[i], _userAddrs[i], _ctxHashes[i], uint64(_ts[i]));
+                emit AccessRequest(now, cobj.libraryAddress(), _contentAddrs[i], _userAddrs[i], _ctxHashes[i], uint64(_ts[i]));
                 if (cobj.contentContractAddress() != 0x0) {
                     bytes32[] memory emptyVals;
                     address[] storage paramAddrs;
@@ -484,7 +487,7 @@ contract BaseContentFactoryExt is BaseContentFactory {
                     c.runAccess(_ts[i], 100, emptyVals, paramAddrs); // TODO: level?
                 }
             } else if (_opCodes[i] == OP_ACCESS_COMPLETE) {
-                emit AccessComplete(cobj.libraryAddress(), _contentAddrs[i], _userAddrs[i], _ctxHashes[i], uint64(_ts[i]));
+                emit AccessComplete(now, cobj.libraryAddress(), _contentAddrs[i], _userAddrs[i], _ctxHashes[i], uint64(_ts[i]));
                 if (cobj.contentContractAddress() != 0x0) {
                     c = Content(cobj.contentContractAddress());
                     c.runFinalizeExt(_ts[i], _amt[i], _userAddrs[i]);

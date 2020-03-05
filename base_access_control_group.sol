9b8d175d2d4f1ec2f398b52f262d2f9e773b6063
Marc-Olivier Lusinchi <marc-olivier@Marc-Oliviers-MacBook-Pro.local>
2020-03-05 11:33:04 -0800

    Overloads checkRights to reflect difference between groups and wallets

--- base_access_control_group.sol
+++ base_access_control_group.sol
@@ -18,12 +18,13 @@ BsAccessCtrlGrp20190722214400ML: Provides the list of members and managers
 BsAccessCtrlGrp20190723130500ML: Fixes typo in managersNum
 BsAccessCtrlGrp20190723165900ML: Fixes deletion/adding to groups
 BsAccessCtrlGrp20200204160600ML: Removes commented out sections
+BsAccessCtrlGrp20200305113000ML: Overloads checkRights to reflect difference between groups and wallets
 */
 
 
 contract BaseAccessControlGroup is AccessIndexor, Editable {
 
-    bytes32 public version ="BsAccessCtrlGrp20200303165900PO"; //class name (max 16), date YYYYMMDD, time HHMMSS and Developer initials XX
+    bytes32 public version ="BsAccessCtrlGrp20200305113000ML"; //class name (max 16), date YYYYMMDD, time HHMMSS and Developer initials XX
 
     //mapping (address => bool) public members;
     //mapping (address => bool) public managers;
@@ -164,6 +165,10 @@ contract BaseAccessControlGroup is AccessIndexor, Editable {
     function canConfirm() public view returns (bool) {
         INodeSpace ns = INodeSpace(contentSpace);
         return ns.canNodePublish(msg.sender);
+    }
+
+    function checkRights(uint8 index_type, address obj, uint8 access_type) public view returns(bool) {
+        return checkDirectRights(index_type, obj, access_type);
     }
 
 }

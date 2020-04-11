2b022f38bf3ae66d5e25a2cf3bfc89c865deb576
Preethi Manoharan <preethi.manoharan@eluv.io>
2020-04-11 10:51:24 -0700

    rm executebatch to meet contract size limit

--- base_content_space.sol
+++ base_content_space.sol
@@ -319,29 +319,7 @@ contract BaseContentSpace is MetaObject, Container, UserSpace, NodeSpace, IKmsSp
         emit RemoveKMSLocator(msg.sender,1);
         return false;
     }
-
-    function executeBatch(uint8[] _v, bytes32[] _r, bytes32[] _s, address[] _from, address[] _dest, uint256[] _value, uint256[] _ts) public {
-
-        require(msg.sender == owner || checkKMSAddr(msg.sender) > 0);
-
-        // TODO: not sure if this is worth it - will just crash if the parameters are passed in incorrectly, which is the same as a revert...?
-        require(_v.length == _r.length);
-        require(_r.length == _s.length);
-        require(_s.length == _from.length);
-        require(_from.length == _dest.length);
-        require(_dest.length == _value.length);
-        require(_value.length == _ts.length);
-
-        for (uint i = 0; i < _v.length; i++) {
-            Transactable t = Transactable(_from[i]);
-            bool success = t.execute(msg.sender, _v[i], _r[i], _s[i], _dest[i], _value[i], _ts[i]);
-
-            if (!success) {
-                // we failed to get the target wallet to pay so - in this scenario - we have to pay!
-                // _dest[i].send(_value[i]); // TODO: transfer? error handling?
-            }
-        }
-    }
+    
 }
 
 

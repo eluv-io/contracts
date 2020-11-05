4d95a783fa83db520517e3bcae802ccee089f823
Preethi Manoharan <preethi.manoharan@eluv.io>
2020-11-04 17:11:11 -0800

    abi-parser, added dist folder

---
+++ abi-parser/gen/gen.go
@@ -0,0 +1,60 @@
+// +build ignore
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+const distDir = "../dist"
+
+type EventStruct struct {
+	Event string `json:"event"`
+	Topic string `json:"topic"`
+}
+
+func main() {
+	files, err := ioutil.ReadDir(distDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		eventsMap := make(map[string]EventStruct)
+		if strings.HasSuffix(f.Name(), ".abi") {
+			jsonBytes, err := ioutil.ReadFile(path.Join(distDir, f.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			parsedABI, err := abi.JSON(bytes.NewReader(jsonBytes))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for e, info := range parsedABI.Events {
+				eventsMap[e] = EventStruct{Event: info.String(), Topic: info.ID().String()}
+			}
+
+			if len(eventsMap) > 0 {
+				out, err := json.Marshal(eventsMap)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				eventsFileName := fmt.Sprintf("%v_events.json", strings.TrimSuffix(f.Name(), ".abi"))
+				err = ioutil.WriteFile(path.Join(distDir, eventsFileName), out, 0755)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+	}
+}
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 sol_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # check the abigen is installed using HomeBrew
-if [ ! -f /usr/local/bin/abigen ]
+if [[ ! -f /usr/local/bin/abigen ]]
 then
 echo "Error : abigen is not found, install ethereum using Homebrew"
 echo "Steps"
@@ -14,15 +14,27 @@ exit 1
 fi
 
 out_dir="$( mkdir -p "$sol_dir/build" && cd "$sol_dir/build" && pwd )"
+abi_dir=$sol_dir/dist
 
 # abigen --sol ./base_content_space.sol --pkg=contracts --out build/base_content_space.go
-$(abigen --sol $sol_dir/base_content_space.sol --pkg=contracts --out $out_dir/base_content_space.go)
-if [ $? -ne 0 ]; then
-echo "error occured while creating go binding!"
+$(abigen --sol ${sol_dir}/base_content_space.sol --pkg=contracts --out ${out_dir}/base_content_space.go)
+if [[ $? -ne 0 ]]; then
+echo "FAILED : error occured while creating go binding!"
 else
-echo "The go binding for base_content_space.sol is present at $out_dir/base_content_space.go"
+echo "SUCCESS : The go binding for base_content_space.sol is present at $out_dir/base_content_space.go"
 fi
 
+out="$(solc ${sol_dir}/base_content_space.sol --abi --hashes --optimize -o ${abi_dir} --overwrite)"
+if [[ $? -ne 0 ]]; then
+echo "FAILED : error occured while running solc"
+echo ${out}
+else
+echo "SUCCESS : ABI and function hashes present in ${abi_dir}"
+fi
 
-
-
+$(go generate ./abi-parser)
+if [[ $? -ne 0 ]]; then
+echo "FAILED : error occured while parsing abi"
+else
+echo "SUCCESS : parsed ABI events present in ${abi_dir}"
+fi

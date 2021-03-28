eaeede178e68634e9556834ec59178b8fd778f90
Paul O'Leary <paul@eluv.io>
2021-03-28 10:52:28 -0700

    WIP - erc721 proxy POC.

--- base_content_space.sol
+++ base_content_space.sol
@@ -19,6 +19,7 @@ import "./meta_object.sol";
 import "./transactable.sol";
 import "./lib_precompile.sol";
 import "./base_tenant_space.sol";
+import "./erc721_proxy.sol";
 
 /* -- Revision history --
 BaseContentSpace20190221114100ML: First versioned released

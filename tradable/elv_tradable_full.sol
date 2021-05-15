c2617797807a261af8513c2d5c5f5485496d16ed
Paul O'Leary <paul@eluv.io>
2021-05-15 10:43:50 -0700

    WIP - base transfer fee, etc.

--- tradable/elv_tradable_full.sol
+++ tradable/elv_tradable_full.sol
@@ -13,6 +13,10 @@ contract ProxyRegistry {
     mapping(address => OwnableDelegateProxy) public proxies;
 }
 
+interface TransferFeeProxy {
+    function getTransferFee(uint256 _tokenId) external view returns (uint256);
+}
+
 /**
  * @title ElvTradable
  * ElvTradable - ERC721 contract that whitelists a trading address, and has minting functionality.
@@ -20,16 +24,18 @@ contract ProxyRegistry {
 contract ElvTradable is ERC721, ERC721Enumerable, ERC721Metadata, MinterRole, Ownable {
     using Strings for string;
 
-    address proxyRegistryAddress;
+    address public proxyRegistryAddress;
 
     constructor(
         string memory _name,
         string memory _symbol,
         string memory _contractURI,
-        address _proxyRegistryAddress
+        address _proxyRegistryAddress,
+        uint256 _baseTransferFee
     ) public ERC721Metadata(_name, _symbol) {
         proxyRegistryAddress = _proxyRegistryAddress;
         contractURI = _contractURI;
+        baseTransferFee = _baseTransferFee;
     }
 
     string public contractURI;
@@ -56,25 +62,76 @@ contract ElvTradable is ERC721, ERC721Enumerable, ERC721Metadata, MinterRole, Ow
      * @dev Burns a specific ERC721 token.
      * @param tokenId uint256 id of the ERC721 token to be burned.
     */
+    // TODO: *don't* allow proxy to burn !?!?! may be irrelevant since the proxy can transfer to an arbitrary account !?!?!
     function burn(uint256 tokenId) public {
-        require(_isApprovedOrOwner(msg.sender, tokenId));
+        require(_isApprovedOrOwner(msg.sender, tokenId) && msg.sender != proxyRegistryAddress);
         _burn(tokenId);
     }
 
     event SetTokenURI(uint256 indexed tokenId, string prevURI, string newURI);
 
     // allows the owner of a token to reset the URI.
+    // TODO: *don't* allow proxy to burn !?!?! may be irrelevant since the proxy can transfer to an arbitrary account !?!?!
     function setTokenURI(uint256 tokenId, string memory uri) public {
-        require(_isApprovedOrOwner(msg.sender, tokenId));
+        require(_isApprovedOrOwner(msg.sender, tokenId) && msg.sender != proxyRegistryAddress);
         emit SetTokenURI(tokenId, this.tokenURI(tokenId), uri);
         _setTokenURI(tokenId, uri);
     }
 
-    event SetProxyAddress(address indexed prevAddr, address indexed newAddr);
+    int public constant PROXY_TYPE_REGISTRY = 1;
+    int public constant PROXY_TYPE_TRANSFER_FEE = 2;
+
+    event SetProxyAddress(int proxyType, address indexed prevAddr, address indexed newAddr);
 
     function setProxyRegistryAddress(address _newProxy) public onlyOwner {
-        emit SetProxyAddress(proxyRegistryAddress, _newProxy);
+        emit SetProxyAddress(PROXY_TYPE_REGISTRY, proxyRegistryAddress, _newProxy);
         proxyRegistryAddress = _newProxy;
+    }
+
+    uint256 public baseTransferFee;
+    address public transferFeeProxyAddress;
+
+    function setTransferFreeProxyAddress(address _newProxy) public onlyOwner {
+        emit SetProxyAddress(PROXY_TYPE_TRANSFER_FEE, transferFeeProxyAddress, _newProxy);
+        transferFeeProxyAddress = _newProxy;
+    }
+
+    event BaseTransferFeeSet(uint256 prevFee, uint256 newFee);
+
+    function setBaseTransferFee(uint256 _newBaseFee) public onlyOwner {
+        emit BaseTransferFeeSet(baseTransferFee, _newBaseFee);
+        baseTransferFee = _newBaseFee;
+    }
+
+    // naming is intended to be in line with proposal: https://github.com/ethereum/EIPs/issues/2665
+    function getTransferFee(uint256 _tokenId) public view returns (uint256) {
+        if (transferFeeProxyAddress == address(0)) {
+            return baseTransferFee;
+        }
+        return TransferFeeProxy(transferFeeProxyAddress).getTransferFee(_tokenId);
+    }
+
+    function withdraw(uint256 _amount) public onlyOwner {
+        msg.sender.transfer(_amount);
+    }
+
+    // TODO: the 'safeTransferFrom' variants in ERC721.sol call 'transferFrom'. I am not 100% sure in Solidity
+    //  if that means they will call into this overridden method by default.
+    function transferFrom(address from, address to, uint256 tokenId) public payable {
+        super.transferFrom(from, to, tokenId);
+        if (msg.value < getTransferFee(tokenId)) {
+            require(isProxyApprovedForAll(ownerOf(tokenId), msg.sender));
+        }
+    }
+
+    function isProxyApprovedForAll(address owner, address operator) public view returns (bool) {
+        if (proxyRegistryAddress != address(0)) {
+            ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+            if (address(proxyRegistry.proxies(owner)) == operator) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -86,13 +143,8 @@ contract ElvTradable is ERC721, ERC721Enumerable, ERC721Metadata, MinterRole, Ow
     view
     returns (bool)
     {
-        if (proxyRegistryAddress != address(0)) {
-            ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
-            if (address(proxyRegistry.proxies(owner)) == operator) {
-                return true;
-            }
-        }
-
+        if (isProxyApprovedForAll(owner, operator))
+            return true;
         return super.isApprovedForAll(owner, operator);
     }
 }

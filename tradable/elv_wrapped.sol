a80ffb050655650719db41615dcf4337f6a96eb5
Paul O'Leary <paul@eluv.io>
2021-06-30 16:56:09 -0700

    New tokens.

--- tradable/elv_wrapped.sol
+++ tradable/elv_wrapped.sol
@@ -26,10 +26,11 @@ contract WELV9 is IERC20 {
     string public symbol   = "WELV";
     uint8  public decimals = 18;
 
-    event  Approval(address indexed src, address indexed guy, uint wad);
-    event  Transfer(address indexed src, address indexed dst, uint wad);
-    event  Deposit(address indexed dst, uint wad);
-    event  Withdrawal(address indexed src, uint wad);
+    // use definitions from IERC20
+    // event Approval(address indexed src, address indexed guy, uint wad);
+    // event Transfer(address indexed src, address indexed dst, uint wad);
+    event Deposit(address indexed dst, uint wad);
+    event Withdrawal(address indexed src, uint wad);
 
     mapping (address => uint)                       public  balanceOf;
     mapping (address => mapping (address => uint))  public  allowance;

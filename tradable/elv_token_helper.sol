7223ff0c2b52cfddfbcfc772cea20c9d93bff6a8
Paul O'Leary <paul@eluv.io>
2021-08-07 15:42:27 -0700

    Make minting helper owner-only. Regen code.

--- tradable/elv_token_helper.sol
+++ tradable/elv_token_helper.sol
@@ -5,7 +5,8 @@ import "./oz_token/ownership/Ownable.sol";
 import "./elv_tradable_full.sol";
 
 contract ElvTokenHelper is Ownable {
-    function mintWithTokenURIMany(address[] memory tok, address[] memory to, uint256[] memory tokenId, string[] memory tokenURI) public returns (bool) {
+    function mintWithTokenURIMany(address[] memory tok, address[] memory to, uint256[] memory tokenId, string[] memory tokenURI)
+    public onlyOwner returns (bool) {
         for (uint i = 0; i < tok.length; i++) {
             ElvTradable(tok[i]).mintWithTokenURI(to[i], tokenId[i], tokenURI[i]);
         }

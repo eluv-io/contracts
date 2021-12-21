145d5e87e061bdb5cee19b2ceb1ffd3f60342d43
paulgoleary <paul@eluv.io>
2021-12-21 15:29:55 -0800

    Add optional hold time override to burn and mint method in helper contract.

--- tradable/elv_token_helper.sol
+++ tradable/elv_token_helper.sol
@@ -16,11 +16,27 @@ contract ElvTokenHelper is Ownable {
         return true;
     }
 
+    uint256 public overrideHoldSecs;
+
+    function setOverrideHoldSecs(uint256 _overrideHoldSecs) public onlyOwner {
+        overrideHoldSecs = _overrideHoldSecs;
+    }
+
     // for this to work the 'from' address needs to be *this* contract
     function burnSignedAndMintMany(address burnAddr, address from, uint256 burnTokenId, uint8 v, bytes32 r, bytes32 s,
         address[] memory tokAddrs, address[] memory to, uint256[] memory tokenIds, string[] memory tokenURIs) public onlyOwner returns (bool) {
         ElvTradable etBurn = ElvTradable(burnAddr);
         etBurn.burnSigned(from, burnTokenId, v, r, s);
-        return mintWithTokenURIMany(tokAddrs, to, tokenIds, tokenURIs);
+        if (overrideHoldSecs != 0) {
+            for (uint i = 0; i < tokAddrs.length; i++) {
+                ElvTradableLocal et = ElvTradableLocal(tokAddrs[i]);
+                if (!et.exists(tokenIds[i])) {
+                    et.mintHoldWithTokenURI(to[i], tokenIds[i], tokenURIs[i], overrideHoldSecs);
+                }
+            }
+            return true;
+        } else {
+            return mintWithTokenURIMany(tokAddrs, to, tokenIds, tokenURIs);
+        }
     }
 }

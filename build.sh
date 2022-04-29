ced42c3c30fb1ff9598d75c7da5240da89c03b73
Peter Tseng <42591958+elv-peter@users.noreply.github.com>
2022-04-28 21:21:24 -0700

    burn many mint many (#86)
    * burn many mint many
    * build with abigen 1.9.11

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-run_solc()
-{
-    bsc_name=$(basename ${1})
-    $(solc ${1} --abi --hashes --optimize -o ${abi_dir} --overwrite)
-    if [[ $? -eq 0 ]]; then
+run_solc() {
+    bsc_name=$(basename "${1}")
+    if solc "${1}" --abi --hashes --optimize -o "${abi_dir}" --overwrite; then
         echo -e "\n SUCCESS : ${bsc_name} ABI and function hashes present in ${abi_dir}"
     fi
 }
@@ -13,39 +11,28 @@ run_solc()
 # to get path to sol folder
 sol_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-separator=
-for i in {0..100}
-do
-  separator+="="
-done
+separator="====================================================================================================="
 
-# check solc
-if [[ ! -f /usr/local/bin/solc ]]
-then
+hash solc || {
     echo "Error : solc is not found"
     exit 1
-fi
+}
 
-# check the abigen is installed using HomeBrew
-if [[ ! -f /usr/local/bin/abigen ]]
-then
-    echo "Error : abigen is not found, install ethereum using Homebrew"
-    echo "Steps"
-    echo "- brew tap ethereum/ethereum"
-    echo "- brew install ethereum"
+# we should also check the version here
+hash abigen || {
+    echo "Error : abigen is not found (must use go-ethereum/cmd/abigen)"
     exit 1
-fi
+}
 
 out_dir="$( mkdir -p "$sol_dir/build" && cd "$sol_dir/build" && pwd )"
 abi_dir=$sol_dir/dist
 
 # abigen --sol ./base_content_space.sol --pkg=contracts --out build/base_content_space.go
-$(abigen --sol ${sol_dir}/base_content_space.sol --pkg=contracts --out ${out_dir}/base_content_space.go)
-if [[ $? -ne 0 ]]; then
+if abigen --sol "${sol_dir}/base_content_space.sol" --pkg=contracts --out "${out_dir}/base_content_space.go"; then
+    echo "SUCCESS : The go binding for base_content_space.sol is present at ${out_dir}/base_content_space.go"
+else
     echo "FAILED : error occured while creating go binding!"
     exit 1
-else
-    echo "SUCCESS : The go binding for base_content_space.sol is present at ${out_dir}/base_content_space.go"
 fi
 echo -e "\n${separator}\n"
 
@@ -58,10 +45,9 @@ echo -e "\n${separator}\n"
 run_solc "${sol_dir}/tradable/elv_tradable_full.sol"
 echo -e "\n${separator}\n"
 
-$(go generate ./cmds/abi-parser)
-if [[ $? -ne 0 ]]; then
+if go generate ./cmds/abi-parser; then
+    echo "SUCCESS : parsed ABI events present in ${abi_dir}"
+else
     echo "FAILED : error occured while parsing abi"
     exit 1
-else
-    echo "SUCCESS : parsed ABI events present in ${abi_dir}"
 fi

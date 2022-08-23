6d9e1461d2683da6bc7ff9204b7563a5ef45bd93
elv-serban <serban@eluv.io>
2022-08-22 21:50:04 -0700

    Initial version of redeemable contract

--- script/tradable/redeemable.s.sol
+++ script/tradable/redeemable.s.sol
@@ -1,21 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.6.12;
 
 import "forge-std/Script.sol";
 import "forge-std/console2.sol";
-import "tradable/redeemable.sol";
+import "src/redeemable.sol";
 
 contract RedeemableScript is Script {
 
     function run() external {
         vm.startBroadcast();
-        Redeemable redeemabpe = new Redeemable();
-
-//        console.log("Creator of the smart contract : ", vm.toString(redeemable.creator()));
-//        console.log("Owner of the smart contract : ", vm.toString(redeemable.owner()));
-
+        Redeemable redeemable = new Redeemable();
         vm.stopBroadcast();
-
-
     }
 }
--- test/tradable/redeemable.t.sol
+++ test/tradable/redeemable.t.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.6.12;
 
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
-import "tradable/redeemable.sol";
+import "src/redeemable.sol";
 
 
 contract RedeemableTest is Test {
     Redeemable redeemable;
-    address adr = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+    address creator = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+    address minter = 0x2233709ecfa91A80626Ff3989d68f67F5B1dD12d;
 
     event RedeemableAdded(uint tokenId);
 
@@ -16,13 +17,45 @@ contract RedeemableTest is Test {
         Set up function, it is the first function executed when we run 'forge test'
     */
     function setUp() public {
-        vm.prank(adr, adr); //adr is the creator and the owner of this smart contract
+        vm.prank(creator, creator); //adr is the creator and the owner of this smart contract
         redeemable = new Redeemable();
     }
 
-    function testRedeemable() public{
-        uint res = redeemable.addRedeemable();
-        assertEq(res, 10, "result != 10");
+    function testAdminRedeemable() public{
+
+        vm.prank(minter);
+        uint res = redeemable.addRedeemableOffer();
+        assertEq(res, 0, "result != 0");
+
+        res = redeemable.addRedeemableOffer();
+        assertEq(res, 1, "result != 1");
+
+        redeemable.removeRedeemableOffer(0);
+
+        vm.expectRevert(bytes("offer not active"));
+        redeemable.removeRedeemableOffer(0);
+
+    }
+
+    function testRedeem() public{
+
+        vm.expectRevert(bytes("offer not active"));
+        redeemable.redeemOffer(1000, 0);
+
+        vm.prank(minter);
+        uint res = redeemable.addRedeemableOffer();
+        assertEq(res, 0, "result != 0");
+
+        redeemable.redeemOffer(1000, 0);
+
+        vm.expectRevert(bytes("offer already redeemed"));
+        redeemable.redeemOffer(1000, 0);
+
+        vm.expectRevert(bytes("offer already redeemed"));
+        redeemable.redeemOffer(1000, 0);
+
+        redeemable.redeemOffer(1001, 0);
+
     }
 
 }
